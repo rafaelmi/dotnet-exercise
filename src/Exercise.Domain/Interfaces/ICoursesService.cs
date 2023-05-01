218132bf0a9e933499b01df738243ba2c196a807
rafaelmi <60661220+rafaelmi@users.noreply.github.com>
2023-05-01 13:38:08 -0400

    add courses service

---
+++ src/Exercise.Domain/Interfaces/ICoursesService.cs
@@ -0,0 +1,16 @@
+﻿using Exercise.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Domain.Interfaces
+{
+    public interface ICoursesService
+    {
+        Task<bool> Create(CourseDTO course);
+        Task<bool> Update(int courseId, CourseDTO course);
+        Task<bool> Delete(int courseId);
+    }
+}
